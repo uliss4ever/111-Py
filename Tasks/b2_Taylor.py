267cd2df23d7ed8593ebcc0721823e41035eca5b
evgeniakalinina <evg5408@yandex.ru>
2020-12-27 20:27:26 +0300

    Tailor

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -4,6 +4,15 @@ Taylor series
 from typing import Union
 
 
+def fact(x: int) -> int:
+    a = 1
+    x = 1
+    while a >= 0:
+        x *= a
+        yield x
+        a += 1
+
+
 def ex(x: Union[int, float]) -> float:
     """
     Calculate value of e^x with Taylor series
@@ -11,8 +20,16 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    n = 50
+    res = 1
+    val = fact(0)
+    for a in range(1, n):
+        res += x**a/next(val)
+        # print(res)
+
+    return round(res, 5)
+    # return 0
+print(ex(5.5))
 
 
 def sinx(x: Union[int, float]) -> float:
