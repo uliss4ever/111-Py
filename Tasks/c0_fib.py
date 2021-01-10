74d9c7620e767fe159c4fd8cb27bd614af6999cd
evgeniakalinina <evg5408@yandex.ru>
2021-01-10 14:57:15 +0300

    fib+

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -1,20 +1,34 @@
 def fib_recursive(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using recursive algorithm
-
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    if n < 1:
+        return 0
+    elif n <= 2:
+        return 1
+    fib = fib_recursive(n-1) + fib_recursive(n-2)
+    return fib
+print(fib_recursive(2))
 
 
 def fib_iterative(n: int) -> int:
     """
     Calculate n-th number of Fibonacci sequence using iterative algorithm
-
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n < 0:
+        raise ValueError
+    fib = [0] * (n + 2)
+    fib[1] = 1
+    for i in range(1, n):
+        fib[i + 1] += fib[i]
+        fib[i + 2] += fib[i]
+
+    return fib[-2]
+print(fib_iterative(0))
+
