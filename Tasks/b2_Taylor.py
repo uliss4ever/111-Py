8eb49b9859883fa1902d988db896baee7274ca17
evgeniakalinina <evg5408@yandex.ru>
2021-01-10 22:51:57 +0300

    factorial++

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -12,39 +12,40 @@ def fact():
         yield x
         a += 1
 g = fact()
-#
-#
-# def ex(x: Union[int, float]) -> float:
-#     """ Calculate value of e^x with Taylor series
-#     :param x: x value
-#     :return: e^x value
-#     """
-#     n = 50
-#     res = 1
-#     val = fact()
-#     for a in range(1, n):
-#         res += x**a/next(val)
-#     return round(res, 5)
-#     # return 0
-# print(ex(5.5))
+
+
+def ex(x: Union[int, float]) -> float:
+    """ Calculate value of e^x with Taylor series
+    :param x: x value
+    :return: e^x value
+    """
+    n = 50
+    res = 1
+    val = fact()
+    for a in range(1, n):
+        res += x**a/next(val)
+    return round(res, 5)
+
+print(ex(5.5))
+
+def fact_for_sin():
+    a = 1
+    x = 1
+    while True:
+        x *= a
+        if a > 2 and a % 2:
+            yield x
+        a += 1
+g = fact_for_sin()
 
 
 def sinx(x: Union[int, float]) -> float:
 
-    """
-    Calculate sin(x) with Taylor series
-
-    :param x: x value
-    :return: sin(x) value
-    """
     n = 50
     res = x
-    val = fact()
-    next(val)
+    g = fact_for_sin()
     for a in range(1, n):
-        next(val)
-        res += ((-1)**a) * (x ** (2*a+1)) / (next(val))
-
+        res += ((-1)**a) * (x ** (2*a+1)) / (next(g))
     return round(res, 5)
 
 
