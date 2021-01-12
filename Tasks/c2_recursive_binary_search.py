e5d3d732af14b68f9c069fac98a055d47ea64bb9
evgeniakalinina <evg5408@yandex.ru>
2021-01-12 19:07:04 +0300

    rec_binary++

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -1,13 +1,38 @@
 from typing import Sequence, Optional
 
 
+def help_req(elem: int, arr: Sequence, left, right):
+    if not arr:
+        return None
+    mid = (left + right) // 2
+    if arr[mid] == elem:
+        while arr[mid - 1] == elem:
+            mid -= 1
+            return mid
+    elif elem < arr[mid]:
+        return help_req(elem, arr, left, mid)
+    elif elem > arr[mid]:
+        return help_req(elem, arr, mid+1, right)
+    return None
+
+
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     """
     Performs binary search of given element inside of array (using recursive way)
-
     :param elem: element to be found
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+    return help_req(elem, arr, 0, len(arr))
+
+
+print(binary_search(1, [0, 1, 2, 2, 3, 3, 4]))
+
+# l = [1, 2, 3, 4, 5]
+# mid = len(l)//2
+# print(mid)
+#
+# left = 0
+# right = len(l) - 1
+# mid1 = (left + right)//2
+# print(mid1)
