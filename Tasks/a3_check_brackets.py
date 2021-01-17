d37278b2ddfea9d7957d38e7eda0de349b5ad8f2
evgeniakalinina <evg5408@yandex.ru>
2021-01-17 18:37:58 +0300

    Скобки ++++

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -17,51 +17,49 @@ def check_brackets(brackets_row: str) -> bool:
             a += 1
         elif i == ")":
             a -= 1
-    print(a)
-    if a < 0:
+    if a != 0:
         return False
     return True
 
+print(check_brackets("())()"))
 
-print(check_brackets(""))
-
-def check_brackets(brackets_row: str) -> bool:
-    if not brackets_row:
-        return True
-    if brackets_row[0] == ")" or brackets_row[-1] == "(":
-        return False
-    for i in brackets_row:
-        if i == "(":
-            stack.push("(")
-        elif i == ")":
-            f = stack.pop()
-        if f == None:
-            return False
-    if stack.pop() != None:
-        return False
-    return True
-
-
-print(check_brackets("()((("))
-
-def check_brackets(brackets_row: str) -> bool:
-    if not brackets_row:
-        return True
-    if brackets_row[0] == ")" or brackets_row[-1] == "(":
-        return False
-    my_stack = []
-    for i in brackets_row:
-        if i == "(":
-            my_stack.insert(0, "(")
-        if i == ")" and my_stack:
-            if my_stack[0] == "(":
-                my_stack.pop()
-        elif i == ")" and not my_stack:
-            my_stack.insert(0, ")")
-
-    if my_stack:
-        return False
-    return True
-
-print(check_brackets("(())())"))
-
+# def check_brackets(brackets_row: str) -> bool:
+#     if not brackets_row:
+#         return True
+#     if brackets_row[0] == ")" or brackets_row[-1] == "(":
+#         return False
+#     for i in brackets_row:
+#         if i == "(":
+#             stack.push("(")
+#         elif i == ")":
+#             f = stack.pop()
+#         if f == None:
+#             return False
+#     if stack.pop() != None:
+#         return False
+#     return True
+#
+#
+# print(check_brackets("()((("))
+#
+# def check_brackets(brackets_row: str) -> bool:
+#     if not brackets_row:
+#         return True
+#     if brackets_row[0] == ")" or brackets_row[-1] == "(":
+#         return False
+#     my_stack = []
+#     for i in brackets_row:
+#         if i == "(":
+#             my_stack.insert(0, "(")
+#         if i == ")" and my_stack:
+#             if my_stack[0] == "(":
+#                 my_stack.pop()
+#         elif i == ")" and not my_stack:
+#             my_stack.insert(0, ")")
+#
+#     if my_stack:
+#         return False
+#     return True
+#
+# print(check_brackets("(())())"))
+#
