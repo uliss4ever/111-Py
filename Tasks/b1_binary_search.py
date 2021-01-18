b780214bb6f0aebb22342ec758cc0d99d2e94f7f
evgeniakalinina <evg5408@yandex.ru>
2021-01-18 11:15:00 +0300

    revent b1

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,20 +9,5 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    if not arr:
-        return None
-    l = 0
-    r = len(arr) - 1
-    while l <= r:
-        mid = (l + r)//2
-        guess = arr[mid]
-        if guess == elem:
-            while arr[mid-1] == elem and mid >= 1:
-                mid -= 1
-            return mid
-        if guess > elem:
-            r = mid - 1
-        else:
-            l = mid + 1
+    print(elem, arr)
     return None
-print(binary_search(2, [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]))
