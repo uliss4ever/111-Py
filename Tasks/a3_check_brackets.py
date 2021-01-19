17bce42f1eb3d5cf929fd4d5b9d5290ff1833f89
evgeniakalinina <evg5408@yandex.ru>
2021-01-19 22:52:33 +0300

    brackets+

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,5 +1,3 @@
-import stack as stack
-
 def check_brackets(brackets_row: str) -> bool:
     """
     Check whether input string is a valid bracket sequence
@@ -7,59 +5,20 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
+
     if not brackets_row:
         return True
-    if brackets_row[0] == ")" or brackets_row[-1] == "(":
-        return False
     a = 0
     for i in brackets_row:
         if i == "(":
             a += 1
         elif i == ")":
             a -= 1
-    if a != 0:
-        return False
-    return True
+        if a < 0:
+            return False
+    if a == 0:
+        return True
 
-print(check_brackets("())()"))
+if __name__ == "__main__":
+    print(check_brackets(")()"))
 
-# def check_brackets(brackets_row: str) -> bool:
-#     if not brackets_row:
-#         return True
-#     if brackets_row[0] == ")" or brackets_row[-1] == "(":
-#         return False
-#     for i in brackets_row:
-#         if i == "(":
-#             stack.push("(")
-#         elif i == ")":
-#             f = stack.pop()
-#         if f == None:
-#             return False
-#     if stack.pop() != None:
-#         return False
-#     return True
-#
-#
-# print(check_brackets("()((("))
-#
-# def check_brackets(brackets_row: str) -> bool:
-#     if not brackets_row:
-#         return True
-#     if brackets_row[0] == ")" or brackets_row[-1] == "(":
-#         return False
-#     my_stack = []
-#     for i in brackets_row:
-#         if i == "(":
-#             my_stack.insert(0, "(")
-#         if i == ")" and my_stack:
-#             if my_stack[0] == "(":
-#                 my_stack.pop()
-#         elif i == ")" and not my_stack:
-#             my_stack.insert(0, ")")
-#
-#     if my_stack:
-#         return False
-#     return True
-#
-# print(check_brackets("(())())"))
-#
